5aeb3f78074a1f4960de5834ee3210828707c189
André Rossner <rossnerandre@gmail.com>
2022-03-17 18:00:29 -0300

    Add servicos de empresas e grupos

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -2,9 +2,36 @@ import { Box, Grid, Paper } from "@mui/material";
 import CardDashboard from "../../components/CardDashboard";
 import { styled, useTheme } from '@mui/material/styles';
 import { ThemeProvider } from "@mui/system";
+import getEmpresas from '../../services/getEmpresas';
+import getGrupos from '../../services/getGrupos';
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
+  let empresasService = getEmpresas();
+  const [empresas, setEmpresas] = useState();
+
+  let gruposService = getGrupos();
+  const [grupos, setGrupos] = useState();
+
+  useEffect(() => {
+    if (empresasService) {
+      setEmpresas(empresasService);
+    }
+    if (gruposService) {
+      setGrupos(gruposService);
+    }
+  })
+
+
   const theme = useTheme();
+
+  if (!empresas || !grupos) {
+    return <p>Loading...</p>
+  }
+  // if (!grupos) {
+  //   return <p>Loading...</p>
+  // }
+
 
   return (<ThemeProvider theme={theme}>
     <Box>
@@ -14,22 +41,24 @@ export default function Dashboard() {
           <Grid item sm={5} md={4} lg={3} xl={2} >
             <CardDashboard
               titulo={'Grupos cadastrados'}
-              quantidade={'32'}
+              quantidade={grupos.length}
               nome={'Empresas'}
-              cardColor={'info.main'}
-              color={'text.white'}
-              btnColor={'primary'}
+              cardColor={'warning.light'}
+              color={'text.dark'}
+              btnColor={'secondary'}
+              link={'/dashboard/grupos'}
             />
           </Grid>
           <Grid item sm={5} md={4} lg={3} xl={2}>
             <CardDashboard
               titulo={'Empresas cadastradas'}
-              quantidade={'32'}
+              quantidade={empresas.length}
               naoseiainda={'Alguma info'}
               nome={'Empresas'}
-              cardColor={'info.light'}
+              cardColor={'info.dark'}
               color={'text.dark'}
-              btnColor={'success'}
+              btnColor={'info'}
+              link={'/dashboard/empresas'}
             />
           </Grid>
         </Grid>
